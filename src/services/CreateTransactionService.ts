3b5b2aa0182abab8f45779bcfe162d5a66b4ea40
gibify <evandrogibicoski@outlook.com>
2020-08-11 17:42:34 -0300

    model an table done

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,11 +1,32 @@
+import { getCustomRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
+import TransactionRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, type, value, category }: Request): Promise<Transaction> {
+    const transactionRepository =  getCustomRepository(TransactionRepository);
+
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
+
+  
 }
 
 export default CreateTransactionService;
